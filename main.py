802c5641d56faaea23d0aa0321e0ae6a4a7c0dc2
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-11-10 10:17:26 +0200

    Add dropdown menu to select conversion mi to km or km to mi and calculate

--- main.py
+++ main.py
@@ -2,32 +2,59 @@ from tkinter import *
 
 
 def button_clicked():
-    miles = miles_input.get()
-    km = round(int(miles) * 1.609344, 3)
-    calculated_km_label.config(text=km)
+    if option.get() == "mi to km":
+        miles = input.get()
+        km = round(float(miles) * 1.609344, 3)
+        calculated_label.config(text=km)
+    elif option.get() == "km to mi":
+        km = input.get()
+        miles = round(float(km) / 1.609344, 3)
+        calculated_label.config(text=miles)
 
 
 window = Tk()
 window.title("Miles to Kilometer Converter")
 window.config(padx=20, pady=20)
 
-miles_label = Label(text="Miles")
-miles_label.grid(column=2, row=0)
+first_label_mi = Label(text="mi")
+first_label_mi.grid(column=2, row=1)
 
-km_label = Label(text="Km")
-km_label.grid(column=2, row=1)
+second_label_km = Label(text="km")
+second_label_km.grid(column=2, row=2)
 
 is_equal_label = Label(text="is equal to")
-is_equal_label.grid(column=0, row=1)
+is_equal_label.grid(column=0, row=2)
 
-calculated_km_label = Label(text="0")
-calculated_km_label.grid(column=1, row=1)
+calculated_label = Label(text="0")
+calculated_label.grid(column=1, row=2)
 
 calculate_button = Button(text="Calculate", command=button_clicked)
-calculate_button.grid(column=1, row=2)
+calculate_button.grid(column=1, row=3)
 
-miles_input = Entry(width=10)
-miles_input.insert(END, string="0")
-miles_input.grid(column=1, row=0)
+input = Entry(width=10)
+input.insert(END, string="0")
+input.grid(column=1, row=1)
+
+option = StringVar()
+
+options = [
+    "mi to km",
+    "km to mi"
+]
+
+
+def mi_or_km(*args):
+    if option.get() == "mi to km":
+        first_label_mi.config(text="mi")
+        second_label_km.config(text="km")
+    elif option.get() == "km to mi":
+        first_label_mi.config(text="km")
+        second_label_km.config(text="mi")
+
+
+option_select = OptionMenu(window, option, *options)
+option.trace("w", mi_or_km)
+option.set(options[0])
+option_select.grid(column=1, row=0)
 
 window.mainloop()
